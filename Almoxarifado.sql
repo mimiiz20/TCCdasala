7c624484b5974e53a4044c1dad31d98ce73aacf0
Mica <micaellagoncalvesbielski@gmail.com>
2026-06-02 09:29:32 -0300

    Alterações no layout e melhorias no sistema

--- Almoxarifado.sql
+++ Almoxarifado.sql
@@ -3,15 +3,18 @@ CREATE DATABASE almoxarifado;
 USE almoxarifado;
 
 CREATE TABLE estoque (
-		id INT PRIMARY KEY AUTO_INCREMENT,
-        nome VARCHAR(255),
-        qtde INT
-        );
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255),
+    qtde INT,
+    imagem VARCHAR(255)
+);
         
 SELECT * FROM estoque;
 
-INSERT INTO estoque (id, nome, qtde)
-VALUES (3, "chave fenda", 12);
-INSERT INTO estoque (id, nome, qtde)
-VALUES (4, "alicate", 8);
-        
+INSERT INTO estoque (nome, qtde, imagem)
+VALUES ("Chave Fenda", 12, "/static/chave_fenda.jpg");
+INSERT INTO estoque (nome, qtde, imagem)
+VALUES ("Alicate", 8, "/static/alicate.jpg");
+
+TRUNCATE TABLE estoque;
+
