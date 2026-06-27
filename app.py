e803697bc1271352e010ee0f528a04c44d77bc6f
mimiiz20 <micaellagoncalvesbielski@gmail.com>
2026-06-27 20:54:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def get_db():
     return mysql.connector.connect(
         host='127.0.0.1',
         user='root',
-        password='Mica@2009',
+        password='',
         database='almoxarifado',
     )
 
